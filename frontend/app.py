fab482d180da45f7ee61fb1eea1e52cafd761a4f
Shaik Mohiddin <shaikmohiddin2809@gmail.com>
2026-07-15 20:40:49 +0530

    Connect frontend to backend

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://personalized-networking-assistant-j93r.onrender.com"
 
 st.title("🤝 Personalized Networking Assistant")
 
